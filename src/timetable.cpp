d20d83a6ee4c1a7ea2edda79939bd4fed6251521
Etern1tyDark <5027231002@student.its.ac.id>
2024-06-07 21:11:15 +0700

    refactor: all classes

--- src/timetable.cpp
+++ src/timetable.cpp
@@ -14,57 +14,84 @@ using namespace std;
 // [x] Polymorphism
 
 class Course {
-    private: // abstraction
+    private:
         string courseName;
         string courseCode;
-        string courseTime;
         string courseDay;
+        string courseTime;
+        Teacher teacher; 
+        vector<Student> students; 
+        Room room; 
     public:
-        // methods to add, remove, update course list
-        Course(string courseName, string courseCode, string courseTime, string courseDay) { // constructor
+        Course(string courseName, Teacher teacher, vector<Student> students, Room room) {
             this->courseName = courseName;
-            this->courseCode = courseCode;
-            this->courseTime = courseTime;
-            this->courseDay = courseDay;
+            this->teacher = teacher;
+            this->students = students;
+            this->room = room;
         }
 
         // setter and getter methods
         void setCourseName(string courseName) { this->courseName = courseName; }
         void setCourseCode(string courseCode) { this->courseCode = courseCode; }
-        void setCourseTime(string courseTime) { this->courseTime = courseTime; }
         void setCourseDay(string courseDay) { this->courseDay = courseDay; }
+        void setCourseTime(string courseTime) { this->courseTime = courseTime; }
+        void setTeacher(Teacher teacher) { this->teacher = teacher; }
+        void setStudents(vector<Student> students) { this->students = students; }
+        void setRoom(Room room) { this->room = room; }
         string getCourseName() { return courseName; }
         string getCourseCode() { return courseCode; }
-        string getCourseTime() { return courseTime; }
         string getCourseDay() { return courseDay; }
+        string getCourseTime() { return courseTime; }
+        Teacher getTeacher() { return teacher; }
+        vector<Student> getStudents() { return students; }
+        Room getRoom() { return room; }
 
-        void addCourse() {
-            // TO DO
+        void addStudent(Student student) {
+            this->students.push_back(student);
         }
 
-        void modifyCourse() {
-            // TO DO
+        void modifyStudent(string studentName, Student student) {
+            for (int i = 0; i < students.size(); i++) {
+                if (students[i].getName() == studentName) {
+                    students[i] = student;
+                    break;
+                }
+            }
         }
 
-        void deleteCourse() {
-            // TO DO
+        void removeStudent(string studentName) {
+            for (int i = 0; i < students.size(); i++) {
+                if (students[i].getName() == studentName) {
+                    students.erase(students.begin() + i);
+                    break;
+                }
+            }
         }
+
+        
 };
 
 class Person {
     protected: // access modifier
         string name;
-        string code;
         string email;
         string phone;
+        vector<string> personList;
     public:
-        Person(string name, string code, string email, string phone) {
+        Person(string name, string email, string phone) {
             this->name = name;
-            this->code = code;
             this->email = email;
             this->phone = phone;
         }
         virtual void printDetails() = 0; // polymorphism
+
+        // setter and getter methods
+        void setName(string name) { this->name = name; }
+        void setEmail(string email) { this->email = email; }
+        void setPhone(string phone) { this->phone = phone; }
+        string getName() { return name; }
+        string getEmail() { return email; }
+        string getPhone() { return phone; }
 
         void addPerson() {
             // TO DO
@@ -80,25 +107,37 @@ class Person {
 };
 
 class Teacher : public Person { // inheritance
+    private:
+        string teacherID;
     public:
-        Teacher(string name, string code, string email, string phone) : Person(name, code, email, phone) {}
+        Teacher(string name, string email, string phone, string teacherID) 
+            : Person(name, email, phone), teacherID(teacherID) {}
+
         void printDetails() override { // overriding
             cout << "Teacher Name: " << name << "\n";
-            cout << "Teacher Code: " << code << "\n";
-            cout << "Teacher Email: " << email << "\n";
-            cout << "Teacher Phone: " << phone << "\n";
+            cout << "Teacher ID: " << teacherID << "\n";
         }
+
+        // setter and getter for teacherID
+        void setTeacherID(string teacherID) { this->teacherID = teacherID; }
+        string getTeacherID() { return teacherID; }
 };
 
 class Student : public Person { // inheritance
+    private:
+        string studentID;
     public:
-        Student(string name, string code, string email, string phone) : Person(name, code, email, phone) {}
+        Student(string name, string email, string phone, string studentID) 
+            : Person(name, email, phone), studentID(studentID) {}
+
         void printDetails() override { // overriding
             cout << "Student Name: " << name << "\n";
-            cout << "Student Code: " << code << "\n";
-            cout << "Student Email: " << email << "\n";
-            cout << "Student Phone: " << phone << "\n";
+            cout << "Student ID: " << studentID << "\n";
         }
+
+        // setter and getter for studentID
+        void setStudentID(string studentID) { this->studentID = studentID; }
+        string getStudentID() { return studentID; }
 };
 
 class Room { 
@@ -106,6 +145,7 @@ class Room {
         string roomNumber;
         int capacity;
         bool isAvailable;
+        vector<string> roomList;
     public:
         // methods to add, remove, update room list
         Room(string roomNumber, int capacity, bool isAvailable) { // constructor
@@ -134,9 +174,24 @@ class Room {
         }
 };
 
-void displayTimetable(string course, string teacher, string student, string room) {
+void displayTimetable(vector<Course> courseList, vector<Teacher> teacherList, vector<Student> studentList, vector<Room> roomList) {
     // TO DO
-}
+    cout << "Generating Available Timetable\n";
+    cout << "-----------------------------\n";
+
+    for (Course course : courseList) {
+        cout << "Course Name: " << course.getCourseName() << "\n";
+        cout << "Course Code: " << course.getCourseCode() << "\n";
+        cout << "Day: " << course.getCourseDay() << "\n";
+        cout << "Time: " << course.getCourseTime() << "\n";
+        cout << "Teacher: " << course.getTeacher().getName() << "\n";
+        cout << "Room: " << course.getRoom().getRoomNumber() << "\n";
+        cout << "Students: \n";
+        for (Student student : course.getStudents()) {
+            cout << student.getName() << "\n";
+        }
+        cout << "-----------------------------\n";
+    }
 
 void printMenu() {
     cout << "1. Course Menu\n";
@@ -158,11 +213,13 @@ void printMenu() {
             cout << "1. Add Course\n";
             cout << "2. Modify Course\n";
             cout << "3. Delete Course\n";
+            cout << "4. Display Course\n";
 
             cout << "Enter your choice: ";
             cin >> courseChoice;
             switch (courseChoice) {
                 // TO DO, USE SETTER AND GETTER
+
             }
             break;
 
@@ -170,11 +227,13 @@ void printMenu() {
             cout << "1. Add Teacher\n";
             cout << "2. Modify Teacher\n";
             cout << "3. Delete Teacher\n";
+            cout << "4. Display Teacher\n";
 
             cout << "Enter your choice: ";
             cin >> teacherChoice;
             switch (teacherChoice) {
                 // TO DO, USE SETTER AND GETTER
+
             }
             break;
 
@@ -182,11 +241,13 @@ void printMenu() {
             cout << "1. Add Student\n";
             cout << "2. Modify Student\n";
             cout << "3. Delete Student\n";
+            cout << "4. Display Student\n";
 
             cout << "Enter your choice: ";
             cin >> studentChoice;
             switch (studentChoice) {
                 // TO DO, USE SETTER AND GETTER
+                
             }
             break;
 
@@ -194,6 +255,7 @@ void printMenu() {
             cout << "1. Add Room\n";
             cout << "2. Modify Room\n";
             cout << "3. Delete Room\n";
+            cout << "4. Display Room\n";
 
             cout << "Enter your choice: ";
             cin >> roomChoice;
@@ -202,9 +264,7 @@ void printMenu() {
             }
             break;
         case 5:
-            cout << "Input Course, Teacher, Student, Room\n";
-            cin >> course >> teacher >> student >> room;
-            displayTimetable(course, teacher, student, room);
+            displayTimetable();
             break;
 
         case 0:
